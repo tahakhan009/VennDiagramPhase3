e9d03c1ce4a78a997bc679c7a15ee70f060ab74f
Taha <57385903+tahakhan009@users.noreply.github.com>
2020-05-26 14:23:07 +0500

    Equation Solving

---
+++ EquationSolving.cs
@@ -0,0 +1,86 @@
+
+#region EquationSolving
+public VennDiagramHelper(string Equation)
+		{
+			_strEquation = Equation;
+		}
+    
+		public Geometry Solve()
+		{
+			FormatEquation(ref _strEquation);
+			_iCircleCount = CalculateCircleCount(_strEquation);
+			CheckCircleCount(_iCircleCount);
+			InitializeOperandVisuals();
+			
+			Geometry result =  ProcessEquation(_strEquation);
+			return result;
+		}
+
+		private Geometry ProcessEquation(string equation)
+		{
+			string equ = equation;
+			equ = EndWithDelim(equ);
+
+			Stack<Geometry> oprn = new Stack<Geometry>();
+			Stack<Operators> oprt = new Stack<Operators>();
+			oprt.Push(Operators.Delim);
+
+			int pointer = 0;
+
+			while (pointer < equ.Count())
+			{
+				Operators curOprt;
+				if (_dictNormalOperators.TryGetValue(equ[pointer], out curOprt))
+				{
+					int curIndex = _oprtOperatorsPriorityIndexer.IndexOf(curOprt);
+					int stackIndex = _oprtOperatorsPriorityIndexer.IndexOf(oprt.Peek());
+					OperatorsPriority op = _oprtOperatorsPriority[stackIndex, curIndex];
+					switch (op)
+					{
+						case OperatorsPriority.Bigger:
+
+							if (oprn.Count < 2)
+							{
+								throw gVennDiagramException.InvalidEquation;
+							}
+
+							Console.WriteLine(curOprt);
+
+							Geometry operand2 = oprn.Pop();
+							Geometry operand1 = oprn.Pop();
+
+							Geometry result = Operate(operand1, operand2, oprt.Pop());
+							oprn.Push(result);
+
+							break;
+						case OperatorsPriority.Smaller:
+							oprt.Push(curOprt);
+							pointer++;
+							break;
+						case OperatorsPriority.Equal:
+							oprt.Pop();
+							pointer++;
+							break;
+						case OperatorsPriority.Invalid:
+							throw gVennDiagramException.InvalidEquation;
+					}
+
+				}
+				else if (_dictSpecialOperators.TryGetValue(equ[pointer],out curOprt))
+				{
+					if (curOprt == Operators.Not)
+					{
+						Geometry operand = oprn.Pop();
+						Geometry result = Operate(operand, Operators.Not);
+						oprn.Push(result);
+						pointer++;
+					}
+				}
+				else
+				{
+					oprn.Push(_dictOperandVisual[equ[pointer]]);
+					pointer++;
+				}
+			}
+			return oprn.Pop();
+		}
